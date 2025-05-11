942209aefd73f040cccf452da6b6502a86ed68bb
Krishna Vamsi <krishnavamsigoli@gmail.com>
2025-05-11 14:50:51 +0530

    final commit

--- docker-image/import.ps1
+++
@@ -1,4 +0,0 @@
-# Import script for Notification Service
-Write-Host "Importing Notification Service Docker image..."
-docker load -i notification-service.tar
-Write-Host "Notification Service image imported successfully!"
